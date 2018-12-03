da125459971b2c673730874ad9e662f6562f723b
Jennifer Strasser <jstrasser@apple.com>
2018-12-03 08:49:00 -0800

    Initial Commit

---
+++ adventOfCode2018/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  adventOfCode2018
+//
+//  Created by Jennifer Strasser on 12/3/18.
+//  Copyright © 2018 Jennifer Strasser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
